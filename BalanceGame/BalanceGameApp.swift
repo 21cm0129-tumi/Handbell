37707f1f9dc695ca048b02c76fe87e1aaa934655
21cm0129-tumi <91646662+21cm0129-tumi@users.noreply.github.com>
2022-08-20 14:31:31 +0900

    Initial Commit

---
+++ BalanceGame/BalanceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceGameApp.swift
+//  BalanceGame
+//
+//  Created by cmStudent on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct BalanceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
